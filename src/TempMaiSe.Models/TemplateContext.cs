58722d8a9ab56112070fafa3c9f7692f168c6219
hangy <hangy@hangy.de>
2023-10-11 20:30:42 +0200

    feat: Add ASP.NET Core Identity sample UI

--- src/TempMaiSe.Models/TemplateContext.cs
+++ src/TempMaiSe.Models/TemplateContext.cs
@@ -1,14 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace TempMaiSe.Models;
 
-public class MailingContext(DbContextOptions<MailingContext> options) : DbContext(options)
+public class MailingContext(DbContextOptions<MailingContext> options) : IdentityDbContext(options)
 {
     public DbSet<Template> Templates { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        ArgumentNullException.ThrowIfNull(modelBuilder);
-        modelBuilder.ApplyConfiguration(new TemplateConfiguration());
+        ArgumentNullException.ThrowIfNull(builder);
+        base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new TemplateConfiguration());
     }
 }
