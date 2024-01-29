fe0dcdd394fd0c89392e316e527031974a3ece3c
hangy <hangy@hangy.de>
2024-01-29 20:04:53 +0100

    deps: Upgrade to jcamp.FluentEmail packages, and simplify sample

--- examples/minimal-api/src/TempMaiSe.Samples.Api/FluentEmailBuilderExtensions.cs
+++ examples/minimal-api/src/TempMaiSe.Samples.Api/FluentEmailBuilderExtensions.cs
@@ -1,5 +1,4 @@
-﻿using FluentEmail.Mailgun;
-using FluentEmail.MailKitSmtp;
+﻿using FluentEmail.MailKitSmtp;
 
 namespace TempMaiSe.Samples.Api;
 
@@ -7,11 +6,8 @@ public static class FluentEmailBuilderExtensions
 {
     private const string Smtp = nameof(Smtp);
     private const string MailKit = nameof(MailKit);
-    private const string MailGun = nameof(MailGun);
-    private const string Mailtrap = nameof(Mailtrap);
-    private const string SendGrid = nameof(SendGrid);
 
-    private static readonly HashSet<string> s_supportedSenders = [ Smtp, MailKit, MailGun, Mailtrap, SendGrid ];
+    private static readonly HashSet<string> s_supportedSenders = [ Smtp, MailKit ];
 
     public static FluentEmailServicesBuilder AddFluentEmail(this IServiceCollection services, ConfigurationManager config)
     {
@@ -48,25 +44,6 @@ public static class FluentEmailBuilderExtensions
                 SmtpClientOptions mailKitOptions = fluentEmailConfigSection.GetRequiredSection(MailKit).Get<SmtpClientOptions>()
                     ?? throw new InvalidOperationException($"Sender '{sender}' requires a valid config section.");
                 builder = builder.AddMailKitSender(mailKitOptions);
-                break;
-            case MailGun:
-                IConfigurationSection mailGunConfigSection = fluentEmailConfigSection.GetRequiredSection(MailGun);
-                string? domainName = mailGunConfigSection.GetValue("ServerName", "example.org")!;
-                string? apiKey = mailGunConfigSection.GetValue("ApiKey", "")!;
-                MailGunRegion region = mailGunConfigSection.GetValue("Region", MailGunRegion.USA);
-                builder = builder.AddMailGunSender(domainName, apiKey, region);
-                break;
-            case Mailtrap:
-                IConfigurationSection mailtrapConfigSection = fluentEmailConfigSection.GetRequiredSection(Mailtrap);
-                string? userName = mailtrapConfigSection.GetValue("UserName", "")!;
-                string? password = mailtrapConfigSection.GetValue("Password", "")!;
-                builder = builder.AddMailtrapSender(userName, password);
-                break;
-            case SendGrid:
-                IConfigurationSection sendGridConfigSection = fluentEmailConfigSection.GetRequiredSection(SendGrid);
-                string? sendGridApiKey = sendGridConfigSection.GetValue("ApiKey", "")!;
-                bool sandBoxMode = sendGridConfigSection.GetValue("SandBoxMode", false)!;
-                builder = builder.AddSendGridSender(sendGridApiKey, sandBoxMode);
                 break;
             default:
                 // Just don't configure a sender. Maybe log a warning about this?
