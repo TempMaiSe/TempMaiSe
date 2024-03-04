9df85fe1b8c849e71a407b55cd35b0c12e395a32
hangy <hangy@hangy.de>
2024-03-05 00:09:29 +0100

    test: Add some unit tests for extension method

---
+++ test/TempMaiSe.Tests/MailServiceExtensionsTests.cs
@@ -0,0 +1,46 @@
+using Fluid;
+
+namespace TempMaiSe.Tests;
+
+public class MailServiceExtensionsTests
+{
+    [Fact]
+    public void AddMailService_Throws_ArgumentNullException_When_Given_Null_ServiceCollection()
+    {
+        // Arrange
+        IServiceCollection? services = null;
+
+        // Act & Assert
+        _ = Assert.Throws<ArgumentNullException>("services", () => MailServiceExtensions.AddMailService(services!));
+    }
+
+    [Fact]
+    public void AddMailService_Can_Be_Called_Without_Parser_Configuration()
+    {
+        // Arrange
+        IServiceCollection services = new ServiceCollection();
+
+        // Act
+        MailServiceExtensions.AddMailService(services);
+
+        // Assert
+        Assert.Single(services.Where(sr => sr.ServiceType.IsAssignableFrom(typeof(FluidParser))));
+    }
+
+    [Fact]
+    public void AddMailService_Calls_Given_Action_To_Configure_Parser()
+    {
+        // Arrange
+        IServiceCollection services = new ServiceCollection();
+        bool called = false;
+        void configureParser(FluidParser _) => called = true;
+
+        // Act
+        MailServiceExtensions.AddMailService(services, configureParser);
+
+        // Assert
+        _ = services.BuildServiceProvider().GetService<FluidParser>(); // Ensure parser is created
+        Assert.Single(services.Where(sr => sr.ServiceType.IsAssignableFrom(typeof(FluidParser))));
+        Assert.True(called);
+    }
+}
